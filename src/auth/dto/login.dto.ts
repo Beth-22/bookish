304a7b988280520b30343e23892cfe3ac4a99633
Beth-22 <nate85here@gmail.com>
2025-01-09 11:58:46 -0500

    Delete src directory

--- src/auth/dto/login.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, MinLength } from "class-validator";
-
-export class LoginDto {
-    @IsNotEmpty({ message: 'Email or username is required' })
-    email_or_username: string; // Either email or username
-
-    @MinLength(8, { message: 'Password must be at least 8 characters' })
-    password: string;
-}
